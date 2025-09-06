7e08b1f13cc40e1e63cb7a00f9589b5a9c7c80cb
Supreet <supreetkumar32@gmail.com>
2025-09-06 07:44:45 +0530

    initial setup

---
+++ src/main/java/com/springbootweekone/weekone/WeekoneApplication.java
@@ -0,0 +1,16 @@
+package com.springbootweekone.weekone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeekoneApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WeekoneApplication.class, args);
+		System.out.println("namaste");
+	}
+
+
+}
---
+++ src/test/java/com/springbootweekone/weekone/WeekoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootweekone.weekone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeekoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
