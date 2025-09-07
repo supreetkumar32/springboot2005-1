c0534771dd0f3eb4665bec89f95a3923098eb854
Supreet <supreetkumar32@gmail.com>
2025-09-07 16:21:57 +0530

    2.4.service layer

--- src/main/java/com/springbootweektwo/weektwo/controllers/EmployeeController.java
+++ src/main/java/com/springbootweektwo/weektwo/controllers/EmployeeController.java
@@ -3,6 +3,7 @@ package com.springbootweektwo.weektwo.controllers;
 import com.springbootweektwo.weektwo.dto.EmployeeDTO;
 import com.springbootweektwo.weektwo.entities.EmployeeEntity;
 import com.springbootweektwo.weektwo.repositories.EmployeeRepository;
+import com.springbootweektwo.weektwo.services.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,31 +13,27 @@ import java.util.Optional;
 @RequestMapping(path="/employees")
 public class EmployeeController {
 
-    private final EmployeeRepository employeeRepository;
+    private final EmployeeService employeeService;
 
-    public EmployeeController(EmployeeRepository employeeRepository){
-        this.employeeRepository=employeeRepository;
-    }
+   public EmployeeController(EmployeeService employeeService){
+       this.employeeService=employeeService;
+   }
 
-//    @GetMapping(path="/getSecretMessage")
-//    public String getMySuperSecretMessage(){
-//        return "secret message";
-//    }
 
     @GetMapping("/{employeeId}")
-    public EmployeeEntity getEmployeeById(@PathVariable(name="employeeId") Long id){
-        return employeeRepository.findById(id).orElse(null);
+    public EmployeeDTO getEmployeeById(@PathVariable(name="employeeId") Long id){
+        return employeeService.getEmployeeById(id);
     }
 
     @GetMapping
-    public List<EmployeeEntity> getAllEmployees(@RequestParam(required=false) Integer age,
+    public List<EmployeeDTO> getAllEmployees(@RequestParam(required=false) Integer age,
                                                 @RequestParam(required=false) String sortBy){
-        return employeeRepository.findAll();
+        return employeeService.getAllEmployees();
     }
 
     @PostMapping
-    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity inputEmployee){
-        return employeeRepository.save(inputEmployee);
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO inputEmployee){
+        return employeeService.createNewEmployee(inputEmployee);
     }
 
     @PutMapping
